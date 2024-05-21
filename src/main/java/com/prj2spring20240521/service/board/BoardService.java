f64995e4aa8395ec3e90cf93e460ea7300772a8d
sebaek <sebaek@gmail.com>
2024-05-21 14:28:21 +0900

    게시물 목록 보기

--- src/main/java/com/prj2spring20240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring20240521/service/board/BoardService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(rollbackFor = Exception.class)
 @RequiredArgsConstructor
@@ -29,5 +31,9 @@ public class BoardService {
             return false;
         }
         return true;
+    }
+
+    public List<Board> list() {
+        return mapper.selectAll();
     }
 }
