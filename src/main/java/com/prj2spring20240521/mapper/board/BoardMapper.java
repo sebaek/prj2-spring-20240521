8855b2823e9f1709f471bb559695411c666cd9f2
sebaek <sebaek@gmail.com>
2024-05-24 10:38:07 +0900

    글 수정시 writer 컬럼 업데이트 코드 삭제

--- src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
@@ -44,8 +44,7 @@ public interface BoardMapper {
     @Update("""
             UPDATE board
             SET title=#{title},
-                content=#{content},
-                writer=#{writer}
+                content=#{content}
             WHERE id=#{id}
             """)
     int update(Board board);
