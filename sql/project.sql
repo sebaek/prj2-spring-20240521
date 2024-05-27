0d73c7a4b6542a01e59676f80c2d1fa9079b6bc9
sebaek <sebaek@gmail.com>
2024-05-27 09:26:30 +0900

    페이지 번호

--- sql/project.sql
+++ sql/project.sql
@@ -70,3 +70,14 @@ CREATE TABLE authority
 
 INSERT INTO authority (member_id, name)
 VALUES (19, 'admin');
+
+USE prj2;
+
+# 게시물 여러개 입력
+INSERT INTO board
+    (title, content, member_id)
+SELECT title, content, member_id
+FROM board;
+SELECT COUNT(*)
+FROM board;
+
