84c3e71d0d1588f13cbe32fa8f8fd752136c10bb
sebaek <sebaek@gmail.com>
2024-05-30 09:20:59 +0900

    게시물 삭제시 좋아요 먼저 삭제

--- src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/board/BoardMapper.java
@@ -170,4 +170,11 @@ public interface BoardMapper {
               AND member_id=#{memberId}
             """)
     int selectLikeByBoardIdAndMemberId(Integer boardId, String memberId);
+
+
+    @Delete("""
+            DELETE FROM board_like
+            WHERE board_id=#{boardId}
+            """)
+    int deleteLikeByBoardId(Integer boardId);
 }
